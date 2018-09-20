889ba3b17ab08c5c2c8179c7196288f5d9eb73c8
Federico Gonzalez <rgonzalezp1115@gmail.com>
2018-09-20 14:33:37 -0500

    Visualization working, need to do a modal that handles errors (give feedback to user)

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,19 @@ class App extends Component {
 constructor(){
     super();
     this.state={
-      json:{}
+      error:false
     };
   }
 
-  myFunction(settings){
-
+myFunction(){
+    try{
+    var spec = JSON.parse(this.divTarget.value);
+    this.setState({error:false})
+  } catch (err){
+    //this checks for malformed JSON, it doesnt check that the attributes are correct
+    this.setState({error:true})
+  }
+    
     var config = {
       // default view background color
       // covers the entire view component
@@ -26,11 +33,23 @@ constructor(){
         tickColor: "red"
       }
     };
-    var spec = {settings};
+
     console.log(spec);
     vegaEmbed(this.barChart, spec, { config: config, tooltip: { theme: 'dark' }, defaultStyle: true }).then(function (result) {
       
     }).catch(console.error);
+  }
+
+  handleChange(event){
+    try{
+    var string = JSON.parse(event);
+    this.setState({json: string});
+  }catch(err){
+
+  }
+}
+  onKeyPress(evt){
+
   }
   
   render() {
@@ -47,18 +66,19 @@ constructor(){
           this.state.json =obj;
           this.divTarget.value=JSON.stringify(obj,null,2)
            }}>toJson</button>
-          <button className="btn btn-primary" onClick={() => {
-          (spec) => this.myFunction(spec)
-           }}>Vega Graph</button>
+          <input type="file"
+      ref={(input) => this.input = input}
+      onKeyPress = {this.onKeyPress.bind(this)}/>
+          <button className="btn btn-primary" onClick={() =>  this.myFunction()
+           }>Vega Graph</button>
           <div className="container-fluid">
           <div className="row">
           <div className="col-xs-12 col-sm-6">
           <textarea
           cols="80"
-          rows="40"
+          rows="30"
           ref= {(div) => this.divTarget = div}
-          onChange={(settings) => this.myFunction(settings)}>
-          Input your JSON
+          placeholder="Input your JSON manually">
           </textarea>
           </div>
           <div className="col-xs-12 col-sm-6">
